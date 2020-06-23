ceddca3d932cb78d31fb8a4daa443f83c83b3141
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-06-22 22:35:19 -0500

    created new repo and refactored solution

---
+++ reverse.js
@@ -0,0 +1,14 @@
+
+function reversal(string) {
+    debugger
+    // var newString = string.split("");
+    // var reverseString = newString.reverse();
+    // var result = reverseString.join("");
+    // // console.log(result);
+    // return result; 
+
+     let newString = string.split('').reverse().join("")
+    return newString
+}
+
+console.log(reversal('happy'));
