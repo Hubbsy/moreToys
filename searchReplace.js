7ce122847dce2c8afdc785f76d9728aab85d089c
Justin Hubbell <hubbsy@justins-mbp.lan>
2020-11-16 15:15:10 -0600

    solved search and replace algo using filter and splice

--- searchReplace.js
+++ searchReplace.js
@@ -12,22 +12,57 @@
 // Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
 
 
-/**Input - 
- * Ouput
- * Constraints 
- * Edge cases
+
+
+
+
+/**Input - string
+ * Ouput - string modified
+ * Constraints  - maintain casing of modified string
+ * Edge cases - 
+ * 
+ * access the input string, seach the string for the before word.  Cut the before word out of the string.  replace the before word with the after word.create the modified string and return it.
+ * 
+ * create an array from the input string
+ * filter the array to return all words not eqaul to before word
+ * store the index of the word being removed
+ * insert the after word into the array at the index of the before word
+ * join the array into a new string
+ * return the new string
+ * 
  */
 
 
 
 
 function myReplace(str, before, after) {
-  return str;
+  let newAfter = after.split('');
+  let caseWord = ''; 
+  let capital = before[0] === before[0].toUpperCase() 
+  if (capital) {
+  caseWord = newAfter[0].toUpperCase() + after.slice(1)
+  } else {
+  caseWord = newAfter[0].toLowerCase() + after.slice(1)
+  }
+  let newStr = ''; 
+  let wordsArray = str.split(' ');
+  let insertIndex = wordsArray.indexOf(before);
+  let subArray = wordsArray.filter((word) => {
+    return word !== before; 
+  });
+  subArray.splice(insertIndex, 0 , caseWord);
+  newStr = subArray.join(' ');
+  return newStr;
 }
 
 
 
-console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
-console.log(myReplace("Let us go to the store", "store", "mall"));
-console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"))
+  
+
+
+
+
+// console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+// console.log(myReplace("Let us go to the store", "store", "mall"));
+// console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"))
 console.log(myReplace("I think we should look up there", "up", "Down"));
