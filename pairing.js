8b64866d4fba3397b1c6b241653c0819519614b9
Justin Hubbell <hubbsy@justins-mbp.lan>
2020-11-16 15:52:45 -0600

    implemented solution for DNA pairing algo, thinking there is more elegant way

--- pairing.js
+++ pairing.js
@@ -10,12 +10,43 @@
 
 // The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 
+/**Input - string
+ * Ouput - array of arrays
+ * Constraints - result must be 2d array
+ * Edge Cases - 
+ * 
+ * access each letter of the input string.  check what the letter is and add its correpsonding match.  store the current letter and its pair in an array, push that array into a result array.  return the result array.    
+ */
 
 function pairElement(str) {
-  return str;
+  let result = [];
+  //split the array into each letter
+  let lettersArray = str.split('');
+  //iterate throug the letters array
+  // console.log(lettersArray)
+  lettersArray.map(letter => {
+    let pairs = []
+    if (letter === "G") {
+      pairs.push(letter, "C")
+    }
+    if (letter === "C") {
+      pairs.push(letter, "G")
+    }
+    if (letter === "A") {
+      pairs.push(letter, "T")
+    }
+    if (letter === "T") {
+      pairs.push(letter, "A")
+    }
+    result.push(pairs)
+  })
+  //check what the current letter is
+  //push the current letter and its matching letter into an array
+  //push the pairs array into the result array
+  return result; 
 }
 
-console.log(pairElement("GCG"));
+console.log(pairElement("GCG")); //should return [["G", "C"], ['C', "G"], ["G", "C"]]
 console.log(pairElement("ATCGA"))// should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]])
 
 console.log(pairElement("TTGAG")) //should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
